5c6e7c072e8a5bf6d5f33c09eaff4c7b3ebe42a9
Aida Yrysbekova <aidiko20@gmail.com>
2024-11-01 19:54:32 -0400

    python functions

---
+++ python-lesson2.py
@@ -0,0 +1,5 @@
+# lambda function is a anonymus single line function usually used for quick operations
+square = lambda x: x ** 2
+print(square(5)) 
+
+#x: is defined in the second line code which is equal to 5
