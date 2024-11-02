6b40a1d7f733b69cb49373d2baf1ee50efe75fef
Aida Yrysbekova <aidiko20@gmail.com>
2024-11-01 20:44:41 -0400

    added file handling

---
+++ python-lesson6.py
@@ -0,0 +1,6 @@
+# with open("example.txt", "w") as file:
+#     file.write("Happy Friday!")
+
+# this line will append following text to example.txt file
+with open("example.txt", "a") as file:
+    file.write("Another text here!\n")
